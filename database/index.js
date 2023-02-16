973e9f6c6249e9a7e1701492829e87d75b04c9fb
JhonG38 <john.tph38@gmail.com>
2023-02-16 09:34:12 +0100

    feat:connexion a la db et creation du model tweet

---
+++ database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://john:john@127.0.0.1:27017/twitter_dwwm', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connexion a la db')
+}).catch(err => {
+    console.log(err)
+})
