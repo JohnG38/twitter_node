c0f165b0a322e1bdf4c2184be2ccd8f5bf7bd6fd
JhonG38 <john.tph38@gmail.com>
2023-02-22 11:25:40 +0100

    feat: mise en place du MVC

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
+const { tweetList } = require('../controllers/tweet.controller');
+
 const router = require('express').Router();
 const tweet = require('./tweet.routes');
-const api = require('./api');
-const Tweet = require('../database/models/Tweet.model');
 
 router.use('/tweet', tweet);
-router.use('/api', api);
 
-router.get('/', (req, res) => {
-
-    Tweet
-        .find({})
-        .exec()
-        .then(tweets => {
-            res.render('tweets/tweet-list', {tweets});
-        })
-})
+router.get('/', tweetList);
 
 module.exports = router;
 
