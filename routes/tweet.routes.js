31c014eb5dfe5f609f5f17dbe9af51d759365493
rachid j <2203618@afpa.fr>
2023-02-27 10:24:12 +0100

    feat: Suppression d'un tweet

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,6 +1,7 @@
-const { createTweet } = require('../controllers/tweet.controller')
+const { createTweet, deleteTweet } = require('../controllers/tweet.controller')
 const router = require('express').Router()
 
-router.post('/new', createTweet) // creation d'un tweet
+router.post('/new', createTweet); // creation d'un tweet
+router.get('/delete/:tweetId', deleteTweet); // supprimer un tweet
 
 module.exports = router
