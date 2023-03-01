02002a7cabcd6287e6346c7adc1d35a404c80849
JhonG38 <john.tph38@gmail.com>
2023-03-01 09:52:37 +0100

    feat: modification d'un tweet

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,10 +1,12 @@
 // route qui va etre en relation avec les actions contenues dans le controller tweet
 
-const { createTweet, deleteTweet } = require('../controllers/tweet.controller');
+const { createTweet, deleteTweet, displayTweet, updateTweet } = require('../controllers/tweet.controller');
 const router = require('express').Router();
 
 
 router.post('/new', createTweet);
 router.get('/delete/:tweetId', deleteTweet );
+router.get('/edit/:tweetId', displayTweet);
+router.post('/edit/:tweetId', updateTweet);
 
 module.exports = router;
