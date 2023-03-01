befd96bcd60d697ca6d0a8125f521fc619049b17
rachid j <2203618@afpa.fr>
2023-03-01 09:51:11 +0100

    feat: Modification d'un tweet

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,7 +1,9 @@
-const { createTweet, deleteTweet } = require('../controllers/tweet.controller')
+const { createTweet, deleteTweet, displayTweet, updateTweet } = require('../controllers/tweet.controller')
 const router = require('express').Router()
 
 router.post('/new', createTweet); // creation d'un tweet
 router.get('/delete/:tweetId', deleteTweet); // supprimer un tweet
+router.get('/edit/:tweetId', displayTweet); // affiche le tweet pour la modification
+router.post('/edit/:tweetId', updateTweet); // Mise a jour du tweet
 
 module.exports = router
