68469df2167832b0818cac656e66a01298f00836
JhonG38 <john.tph38@gmail.com>
2023-03-02 09:33:26 +0100

    feat: empecher la modification d'un tweet

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,14 @@ const router = require('express').Router();
 
 const tweetRoute = require('./tweet.routes');
 const userRoute = require('./user.routes');
-
-const authRoute = require('./auth.route');
-
+const authRoute = require('./auth.routes');
 const { tweetList } = require('../controllers/tweet.controller');
 const { ensureAuthenticated } = require('../config/security.config');
+
 
 router.use('/tweet', ensureAuthenticated, tweetRoute);
 router.use('/user', userRoute);
 router.use('/auth', authRoute);
-
 router.get('/', tweetList);
 
 
